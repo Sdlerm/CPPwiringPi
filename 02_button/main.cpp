6c3d477601d797d1c0b629bc7c2698c3644b8c91
Samuel Lerman <sdlerm@gmail.com>
2026-09-04 13:51:24 -0400

    Enhance button press detection in main.cpp and add release message; update debounce delay

--- 02_button/main.cpp
+++ 02_button/main.cpp
@@ -17,13 +17,23 @@ int main() {
     // Main loop to detect button presses
     while (true) {
         int now = digitalRead(BTN); // read the current state of the button
+        
         if (now == LOW && prev == HIGH) //detect exactly one edge (= change in state?); now is the current state and prev is the previous state
         {
             std::cout << "PUSHED MOTHERFUCKER!\n";
         }
+        //Message for when button is released
+        
+        if (now == HIGH && prev == LOW)
+        {
+            std::cout << "RELEASED COCKSUCKER!\n";
+        }
         prev = now; // update the previous state to the current state; high --(button pushed)--> low; prev is now assigned to the current state of the button
-        delay(5); //debounce-ish poll interval; prevents detecting multiple presses too quickly
+        delay(10); //debounce-ish poll interval; prevents detecting multiple presses too quickly
+
     }
+
+
 
     return 0;
 }
